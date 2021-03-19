3daf70fe3a1a2750af032d4db49e58759a676b0e
Nayem <nayemk900@gmail.com>
2021-03-19 22:04:40 +0600

    Home page Style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,10 +6,15 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div className="container home row">
-            {
-                FakeData.map(rider => <RidersName rider ={rider} key ={rider.id}></RidersName>)
-            }   
+        <div className="container home ">
+            <h3 className="text-center">Welcome To Travel Ride</h3>
+            <h1 className="text-center">You can take a ride of your choice..</h1>
+            <div className="row">
+                {
+                    FakeData.map(rider => <RidersName rider={rider} key={rider.id}></RidersName>)
+                }
+            </div>
+
         </div>
     );
 };
